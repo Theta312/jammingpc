1f7d4c1e12e752dc6040d010403483c297fa4768
Theta312 <114621804+Theta312@users.noreply.github.com>
2024-07-19 15:04:36 -0400

    created SearchResult and made the button to add the song to playlist useState

--- src/Components/SearchResults/SearchResult.js
+++ src/Components/SearchResults/SearchResult.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
 
 
 export default function SearchResult({handlePlaylist, searchResult}) {
 
-    
-
-
-
-
 
     return (
         <div>
-        
-        
+            <div>
+                <h3>Search Results</h3>
+            </div>
+            <div>
+                {
+                    searchResult.map(song => {
+                        return (
+                            <div key={song.id}>
+                                <h4>{song.name}</h4>
+                                <p>By: {song.artist}</p>
+                                <p>Album: {song.album}</p>
+                                <button type='button' onClick={() => handlePlaylist(song)} >+</button>                                
+                            </div>
+                        )
+                    })
+                }
+            </div>        
         
         </div>
     )
